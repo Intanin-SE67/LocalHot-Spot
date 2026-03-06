c5ea163597426ce6a544122db8ce13fdbf3db3d8
Intanin-SE67 <67023097@up.ac.th>
2026-03-06 18:20:28 +0700

    Fix Api/Delete

--- app/api/delete/[id]/route.ts
+++ app/api/delete/[id]/route.ts
@@ -1,12 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest,NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function DELETE(
-    request: Request,{ params }: { params: { id: string } }
-){
-    const { id } = await params;
-    await prisma.create.delete({
-        where: { id: Number(id) }
-    });
-    return NextResponse.json({ message: "Deleted successfully" });
+    request: NextRequest,{ params }: { params: { id: string } }
+):Promise<NextResponse>{
+    const { id } = params;
+  return NextResponse.json({ message: `Deleted item ${id} Successfully` });
 }
