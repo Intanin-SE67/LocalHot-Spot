02a0ec3d916b0a99d9400eccda4e6eadacb5d102
Intanin-SE67 <67023097@up.ac.th>
2026-03-06 16:50:50 +0700

    fix navbar linking

--- app/Profile/layout.tsx
+++ app/Profile/layout.tsx
@@ -3,7 +3,7 @@ import {useState} from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Category from "../components/Category";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
